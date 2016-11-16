c9f11136ca254c50551f872791c3fcc2d01e816f
Meowz <2601781748@qq.com>
2016-11-16 23:03:45 +0800

    fix bug

--- app/src/main/java/com/meowzv/zhihureader/model/ImageWithTextBean.java
+++ app/src/main/java/com/meowzv/zhihureader/model/ImageWithTextBean.java
@@ -5,4 +5,27 @@ package com.meowzv.zhihureader.model;
  */
 
 public class ImageWithTextBean {
+    private String imageUrl;
+    private String imageWithText;
+
+    public ImageWithTextBean(String imageUrl, String imageWithText) {
+        setImageUrl(imageUrl);
+        setImageWithText(imageWithText);
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageWithText() {
+        return imageWithText;
+    }
+
+    public void setImageWithText(String imageWithText) {
+        this.imageWithText = imageWithText;
+    }
 }
